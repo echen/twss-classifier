f894d13f7d3660ed3c854879dce0c1b7d421c50f
Edwin Chen <iskaldur@gmail.com>
2011-05-04 22:41:32 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# What?!
+
+A Naive Bayes model built for That's What She Said classification.
+
+	nb = NaiveBayes.new(1, POS_TRAINING_EXAMPLES, NEG_TRAINING_EXAMPLES) # train a unigram model
+	if nb.classify("that was longer than i expected") > 0.99
+		puts "That's what she said!"
+	end
+	
+Read more about the algorithm [here](http://blog.echen.me).
---
+++ demo.rb
@@ -0,0 +1,23 @@
+require File.expand_path('../naive-bayes', __FILE__)
+
+ALL_POS_EXAMPLES = IO.readlines("data/twss-stories-parsed.txt")
+ALL_NEG_EXAMPLES_FMYLIFE = IO.readlines("data/fmylife-parsed.txt")
+ALL_NEG_EXAMPLES_TFLN = IO.readlines("data/texts-from-last-night-parsed.txt")
+
+POS_TRAINING_EXAMPLES = ALL_POS_EXAMPLES.first(1000)
+NEG_TRAINING_EXAMPLES = ALL_NEG_EXAMPLES_FMYLIFE.first(500) + ALL_NEG_EXAMPLES_TFLN.first(500)
+
+POS_TEST_EXAMPLES = ALL_POS_EXAMPLES.last(1000)
+NEG_TEST_EXAMPLES = ALL_NEG_EXAMPLES_FMYLIFE.last(500) + ALL_NEG_EXAMPLES_TFLN.last(500)
+
+nb = NaiveBayes.new(1, POS_TRAINING_EXAMPLES, NEG_TRAINING_EXAMPLES)
+nb.train
+
+false_positives = 0
+total_count = 0
+File.read("data/gutenberg-fairy-tales.txt").split(".").select{ |x| !x.strip.empty? }.map(&:strip).each do |line|
+  false_positives += 1 if nb.classify(line) > 0.99
+  total_count += 1
+end
+puts false_positives
+puts total_count
